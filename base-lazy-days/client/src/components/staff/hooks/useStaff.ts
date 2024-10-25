23a5b5db3fd70eeb60a307f901d510d60ec02d90
minjae <minjae2271@gmail.com>
2024-10-25 11:23:30 +0200

    refetch

--- base-lazy-days/client/src/components/staff/hooks/useStaff.ts
+++ base-lazy-days/client/src/components/staff/hooks/useStaff.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import { useQuery } from "@tanstack/react-query";
 
 import type { Staff } from "@shared/types";
@@ -18,11 +18,17 @@ export function useStaff() {
   // for filtering staff by treatment
   const [filter, setFilter] = useState("all");
 
+  const selectFn = useCallback((data: Staff[], filter: string) => {
+    if(filter === 'all') return data
+    return filterByTreatment(data, filter)
+  }, [filter])
+
   const fallback: Staff[] = []
   // TODO: get data from server via useQuery
   const { data: staff = fallback } = useQuery({
     queryKey: [queryKeys.staff],
     queryFn: getStaff,
+    select: (data) => selectFn(data, filter)
   });
 
   return { staff, filter, setFilter };
